852fa4a3d86e03929c46d122b166abf92ddf8a37
Josh Smith <jrsmith9822@gmail.com>
2016-09-03 19:28:47 +0100

    Working chat client

--- src/Client.py
+++ src/Client.py
@@ -1,10 +1,79 @@
+import json
+import socket
+import select
+import sys
 
 
 class Client():
     def __init__(self):
-        super().__init__()
+        self.sock = socket.socket()
+        self.client_name = ''
+        self.server_port = 5000
+        self.server_address = ''
+        self.running = False
+
+    def start(self):
+        self.running = True
+        connected = False
+        self.server_address = input("Enter Server IP: ")
+        self.sock.connect((self.server_address, self.server_port))
+        self.client_name = input("Enter Name: ")
+        while not connected:
+            #try
+
+            self.sock.send(self.client_name.encode('utf-8'))
+
+            response = self.sock.recv(1024).decode('utf-8')
+
+            json_data = json.loads(response)
+            print(json_data['message'])
+            if json_data['type'] == 'success':
+                connected = True
+            else:
+                self.client_name = input("Enter Name: ")
+        self.run()
+
+    def run(self):
+        inputs = [self.sock, sys.stdin]
+        while self.running:
+            inputready,_,_ = select.select(inputs,[],[])
+
+            for s in inputready:
+                if s == sys.stdin:
+                    user_input = sys.stdin.readline()
+                    if user_input.startswith('/'):
+                        if user_input.lower().startswith('/msg'):
+                            split_user_input = user_input.split(' ')
+                            to = split_user_input[1]
+                            text = " ".join(split_user_input[2:])
+                            message = {'type': 'message', 'to': to, 'message': text}
+                            json_message = json.dumps(message)
+                            self.sock.send(json_message.encode('utf-8'))
+                        elif user_input.lower() == '/exit\n':
+                            self.stop()
+                            sys.exit(0)
+
+                if s == self.sock:
+                    received = s.recv(1024).decode('utf-8')
+                    json_data = json.loads(received)
+                    if json_data['type'] == 'message':
+                        print("From {}: {}".format(json_data['from'], json_data['message']))
+                    elif json_data['type'] == 'error':
+                        print("ERROR: " + json_data['message'])
+
+
+
+
+    def stop(self):
+        self.running = False
+        self.sock.close()
+        sys.exit(0)
+
+
+
 
 
 
 if __name__ == "__main__":
     c = Client()
+    c.start()
--- src/Server.py
+++ src/Server.py
@@ -2,25 +2,35 @@ import socket
 import threading
 from Crypto.Cipher import AES
 import json
+import select
 
 
 class ConnectionThread(threading.Thread):
-    def __init__(self, conn, addr, group=None, target=None, name=None, args=(), kwargs=None, *, daemon=None):
-        super().__init__(group, target, name, args, kwargs, daemon=daemon)
+    def __init__(self, name, conn, addr):
+        super().__init__()
         self.connection = conn
-        self.connection.settimeout(300)  # 5 minutes
+        self.connection.settimeout(None)
         self.client_address = addr
-        self.client_name = None
+        self.client_name = name
         self.__running = False
 
     def run(self):
         disconnected = False
         while self.__running and not disconnected:
             try:
-                received = self.connection.recv(1024)
-                data = json.loads(received)
-                print("data received from {0} at {1} going to {2}".format(self.client_name, self.client_address, data['to']))
-                print("It says: ", data['message'])
+                received = ''
+                while len(received) == 0:
+                    received = self.connection.recv(1024).decode('utf-8')
+                json_data = json.loads(received)
+                print("data received from {0} at {1} going to {2}".format(self.client_name, self.client_address, json_data['to']))
+                print("It says: ", json_data['message'])
+                if s.connection_threads.get(json_data['to']) is None:
+                    response = {'type': 'error', 'message': 'No user connected with this name!'}
+                    self.connection.send(json.dumps(response).encode('utf-8'))
+                else:
+                    response = json_data
+                    response['from'] = self.client_name
+                    s.connection_threads[response['to']].connection.send(json.dumps(response).encode('utf-8'))
             except BrokenPipeError:
                 print("Client at {0} has disconnected".format(self.client_address))
                 disconnected = True
@@ -39,7 +49,7 @@ class Server:
         super().__init__()
         self.running = False
         self.HOST = '127.0.0.1'
-        self.PORT = 21
+        self.PORT = 5000
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # debug
         self.connection_threads = {}
@@ -54,18 +64,19 @@ class Server:
 
     def listen(self):
         self.sock.listen(5)
+
         while self.running:
             conn, addr = self.sock.accept()
             conn.settimeout(5)
             try:
-                name = conn.recv(1024)
+                name = conn.recv(1024).decode('utf-8')
                 if self.connection_threads.get(name) is None:
                     response = {'type': 'success', 'message': 'Name Granted! You are known as {}.'.format(name)}
                     conn.send(json.dumps(response).encode('utf-8'))
+                    print("Connection from: {0}@{1}".format(name, addr))
                     thread = ConnectionThread(name, conn, addr)
                     self.connection_threads[name] = thread
                     thread.start()
-                    print("Connection from: {0}@{1}".format(name, addr))
                 else:
                     response = {'type': 'error', 'message': 'Name Already Taken!'}
                     conn.send(json.dumps(response).encode('utf-8'))
