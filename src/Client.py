075d0dcafb4e2eca90af6cd4993efa9514845a42
Josh Smith <jrsmith9822@gmail.com>
2016-09-06 14:12:55 +0100

    Working encryption with static key

--- src/Client.py
+++ src/Client.py
@@ -2,9 +2,15 @@ import json
 import socket
 import select
 import sys
+from Crypto.Cipher import AES
+from Crypto import Random
+import hashlib
+import base64
+import binascii
+password = "HeyItsMeYourBrother".encode()
 
 
-class Client():
+class Client:
     def __init__(self):
         self.sock = socket.socket()
         self.client_name = ''
@@ -36,7 +42,7 @@ class Client():
     def run(self):
         inputs = [self.sock, sys.stdin]
         while self.running:
-            inputready,_,_ = select.select(inputs,[],[])
+            inputready, _, _ = select.select(inputs, [], [])
 
             for s in inputready:
                 if s == sys.stdin:
@@ -46,7 +52,8 @@ class Client():
                             split_user_input = user_input.split(' ')
                             to = split_user_input[1]
                             text = " ".join(split_user_input[2:])
-                            message = {'type': 'message', 'to': to, 'message': text}
+                            cipher_text, iv = encrypt_message(text)
+                            message = {'type': 'message', 'to': to, 'iv': iv, 'message': cipher_text}
                             json_message = json.dumps(message)
                             self.sock.send(json_message.encode('utf-8'))
                         elif user_input.lower() == '/exit\n':
@@ -57,12 +64,11 @@ class Client():
                     received = s.recv(1024).decode('utf-8')
                     json_data = json.loads(received)
                     if json_data['type'] == 'message':
-                        print("From {}: {}".format(json_data['from'], json_data['message']))
+                        print("From {}: {}".format(json_data['from'],
+                                                   decrypt_message(json_data['message'],
+                                                                   json_data['iv'])))
                     elif json_data['type'] == 'error':
                         print("ERROR: " + json_data['message'])
-
-
-
 
     def stop(self):
         self.running = False
@@ -70,10 +76,31 @@ class Client():
         sys.exit(0)
 
 
+def encrypt_message(text):
+    key = hashlib.sha256(password).digest()
+    iv = Random.new().read(AES.block_size)
+    enc = AES.new(key, AES.MODE_CBC, iv)
+    if len(text) % 16 != 0:
+        text += ' ' * (16 - len(text) % 16)
+    text = text.encode('utf-8') # encode utf-8
+    cipher_text = enc.encrypt(text)
+    #print(len(cipher_text)) # dedbug
+    ## padd for decoding
+    return binascii.hexlify(cipher_text).decode('utf-8'), binascii.hexlify(iv).decode('utf-8')
 
 
+def decrypt_message(cipher_text, iv):
+    cipher_text = binascii.unhexlify(cipher_text.encode('utf-8'))
+    iv = binascii.unhexlify(iv.encode('utf-8'))
+    key = hashlib.sha256(password).digest()
+    dec = AES.new(key, AES.MODE_CBC, iv)
+
+    return dec.decrypt(cipher_text).decode().strip()
+
+## utf-8 encode -> encrypt -> b64 encode -> send -> b64 decode -> decrypt -> utf-8 decode
 
 
 if __name__ == "__main__":
     c = Client()
     c.start()
+
